f704c637b236d4c7a9f98e7cb919148f6eb6c855
Anton Kryzhanivskyi <firechant2@gmail.com>
2025-07-05 19:21:42 +0300

    refactoring code

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -9,7 +9,10 @@ export function getAll(): Promise<Good[]> {
 
 export const get5First = () => {
   return getAll().then(goods =>
-    goods.slice(0, 5).sort((a, b) => a.name.localeCompare(b.name)),
+    goods
+      .slice()
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .slice(0, 5),
   ); // sort and get the first 5
 };
 
